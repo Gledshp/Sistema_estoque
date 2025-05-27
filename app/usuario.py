3a7f1e1fef16ca7f9bde7a1b109a36c8fa08124d
Gledshp <gledson.hpaiva@hotmail.com>
2025-05-27 16:58:55 -0300

    Delete app/usuario.py

--- app/usuario.py
+++
@@ -1,9 +0,0 @@
-# Aqui você pode colocar as classes modelo se quiser. Por enquanto não é usado diretamente, pois usamos DBManager para acesso.
-class Usuario:
-    def __init__(self, id, nome, login, email, senha, nivel):
-        self.id = id
-        self.nome = nome
-        self.login = login
-        self.email = email
-        self.senha = senha
-        self.nivel = nivel
