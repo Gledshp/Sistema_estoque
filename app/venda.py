5bc50f6dd02e80f635f0836a6ed1cce6784f0d1a
Gledshp <gledson.hpaiva@hotmail.com>
2025-05-27 16:58:33 -0300

    Delete app/venda.py

--- app/venda.py
+++
@@ -1,6 +0,0 @@
-class Venda:
-    def __init__(self, id, produto_id, quantidade, data):
-        self.id = id
-        self.produto_id = produto_id
-        self.quantidade = quantidade
-        self.data = data
