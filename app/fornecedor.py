311cb94ee21cf8131175c2c4c032890bdbd829e4
Gledshp <gledson.hpaiva@hotmail.com>
2025-05-27 16:58:19 -0300

    Delete app/fornecedor.py

--- app/fornecedor.py
+++
@@ -1,7 +0,0 @@
-class Fornecedor:
-    def __init__(self, id, nome, contato, telefone, email):
-        self.id = id
-        self.nome = nome
-        self.contato = contato
-        self.telefone = telefone
-        self.email = email
